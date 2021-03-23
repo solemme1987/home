a5b1d6f6c6cc37ea0d4f2153ed61a7af75c1ddde
lplugof62 <lplugof@gmail.com>
2021-03-23 12:10:58 -0500

    adición footer

--- views/components/footer.php
+++ views/components/footer.php
@@ -1,1 +1,226 @@
-<h2>Soy un footer</h2>
+    <!-- ====== Contact Area ====== --> 
+    <div class="contact-area">
+        <div class="container-large-device">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="heading-content-two available">
+                            <h2 class="title">We Are Available<br/> For You 24/7</h2>
+                            <h5 class="sub-title">Our online support service is always 24 Hours</h5>
+                        </div><!-- /.testimonial-heading-content -->
+                    </div><!-- /.col-md-12 -->
+                </div><!-- /.row -->
+                <div class="row">
+                    <div class="col-md-7">
+                        <div class="map-left-content">
+                            <iframe width="600" height="350" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC871wKM6aoCLSV_pT3xBVsYzNGXaDh7Pw&q=121King+St,Melbourne+VIC+3000,Australia" allowfullscreen="allowfullscreen"></iframe>
+                        </div><!-- /.mapl-left-content -->
+                    </div><!-- /.col-md-7 -->
+                    <div class="col-md-5">
+                        <div class="map-right-content">
+                            <div class="row">
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="contact">
+                                        <h4><i class="fa fa-address-book"></i>Address</h4>
+                                        <p>112/B - Road 121, King/St Melbourne Australia</p>
+                                    </div><!-- /.contact -->
+                                </div><!-- /.col-md-6 -->
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="contact">
+                                        <h4><i class="fa fa-envelope"></i>Mail</h4>
+                                        <p>yourmail@domain.com houserent@domain.com</p>
+                                    </div><!-- /.contact -->
+                                </div><!-- /.col-md-6 -->
+                            </div><!-- /.row -->
+                            <div class="row">
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="contact">
+                                        <h4><i class="fa fa-phone-square"></i>Call</h4>
+                                        <p>+99 0215469875 <br/>666 35874692050</p>
+                                    </div><!-- /.contact -->
+                                </div><!-- /.col-md-6 -->
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="contact">
+                                        <h4><i class="fa fa-user-circle"></i>Social account</h4>
+                                        <div class="social-icon">
+                                            <a href="#"><i class="fa fa-facebook"></i></a>
+                                            <a href="#"><i class="fa fa-twitter"></i></a>
+                                            <a href="#"><i class="fa fa-instagram"></i></a>
+                                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                                        </div><!-- /.Social-icon -->
+                                    </div><!-- /.contact -->
+                                </div><!-- /.col-md-6 -->
+                            </div><!-- /.row -->
+                        </div><!-- /.map-right-content -->
+                    </div><!-- /.col-md-5 -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+    </div><!-- /.contact-area -->
+
+    <!-- ====== Blog Area ====== --> 
+    <div class="blog-area bg-gray-color">
+        <div class="container">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="heading-content-one">
+                        <h2 class="title">Our Blog Post</h2>
+                        <h5 class="sub-title">Our News Feed</h5>
+                    </div><!-- /.blog-heading-content -->
+                </div><!-- /.row -->
+            </div><!-- /.col-md-12 -->
+            <div class="row">
+                <div class="col-md-4 col-sm-6 col-xs-6">
+                    <article class="post">
+                        <figure class="post-thumb">
+                            <a href="blog-single.html">
+                                <img src="assets/images/blog/blog-one.png" alt="blog" />
+                            </a>
+                        </figure><!-- /.post-thumb -->
+                        <div class="post-content">  
+                            <div class="entry-meta">
+                                <span class="entry-date">
+                                    July 25, 2016
+                                </span>
+                                <span class="devied"></span>
+                                <span class="entry-category">
+                                    <a href="#">Rooms &amp; suites</a>
+                                </span>
+                            </div><!-- /.entry-header -->
+                            <div class="entry-header">
+                                <h3><a href="blog-single.html">Finding best places to visit in California</a></h3>
+                            </div><!-- /.entry-header -->
+                            <div class="entry-footer">
+                                <div class="entry-footer-meta">
+                                    <span class="view"><i class="fa fa-eye"></i>35</span>
+                                    <span class="like"><a href="#"><i class="fa fa-heart-o"></i>09</a></span>
+                                    <span class="comments"><a href="#"><i class="fa fa-comments"></i>05</a></span>
+                                </div><!-- /.entry-footer-meta -->
+                            </div><!-- /.entry-footer -->
+                        </div><!-- /.post-content -->
+                    </article><!-- /.post -->
+                </div><!-- /.col-md-4 -->
+                <div class="col-md-4 col-sm-6 col-xs-6">
+                    <article class="post">
+                        <figure class="post-thumb">
+                            <a href="blog-single.html">
+                                <img src="assets/images/blog/blog-two.png" alt="blog" />
+                            </a>
+                        </figure><!-- /.post-thumb -->
+                        <div class="post-content">  
+                            <div class="entry-meta">
+                                <span class="entry-date">
+                                    July 25, 2016
+                                </span>
+                                <span class="devied"></span>
+                                <span class="entry-category">
+                                    <a href="#">Rooms &amp; suites</a>
+                                </span>
+                            </div><!-- /.entry-header -->
+                            <div class="entry-header">
+                                <h3><a href="blog-single.html">Finding best places to visit in California</a></h3>
+                            </div><!-- /.entry-header -->
+                            <div class="entry-footer">
+                                <div class="entry-footer-meta">
+                                    <span class="view"><i class="fa fa-eye"></i>35</span>
+                                    <span class="like"><a href="#"><i class="fa fa-heart-o"></i>09</a></span>
+                                    <span class="comments"><a href="#"><i class="fa fa-comments"></i>05</a></span>
+                                </div><!-- /.entry-footer-meta -->
+                            </div><!-- /.entry-footer -->
+                        </div><!-- /.post-content -->
+                    </article><!-- /.post -->
+                </div><!-- /.col-md-4 -->
+                <div class="col-md-4 col-sm-6 col-xs-6">
+                    <article class="post">
+                        <figure class="post-thumb">
+                            <a href="blog-single.html">
+                                <img src="assets/images/blog/blog-three.png" alt="blog" />
+                            </a>
+                        </figure><!-- /.post-thumb -->
+                        <div class="post-content">  
+                            <div class="entry-meta">
+                                <span class="entry-date">
+                                    July 25, 2016
+                                </span>
+                                <span class="devied"></span>
+                                <span class="entry-category">
+                                    <a href="#">Rooms &amp; suites</a>
+                                </span>
+                            </div><!-- /.entry-header -->
+                            <div class="entry-header">
+                                <h3><a href="blog-single.html">Finding best places to visit in California</a></h3>
+                            </div><!-- /.entry-header -->
+                            <div class="entry-footer">
+                                <div class="entry-footer-meta">
+                                    <span class="view"><i class="fa fa-eye"></i>35</span>
+                                    <span class="like"><a href="#"><i class="fa fa-heart-o"></i>09</a></span>
+                                    <span class="comments"><a href="#"><i class="fa fa-comments"></i>05</a></span>
+                                </div><!-- /.entry-footer-meta -->
+                            </div><!-- /.entry-footer -->
+                        </div><!-- /.post-content -->
+                    </article><!-- /.post -->
+                </div><!-- /.col-md-4 -->
+            </div><!-- /.row -->
+            <a href="blog.html" class="button">show all</a>
+        </div><!-- /.container -->
+    </div><!-- /.Blog-area-->
+
+    <!-- ====== Footer Area ====== --> 
+    <footer class="footer-area" style="background-image:url(assets/images/footer-bg.png)">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-4">
+                   <div class="widget widget_about yellow-color">    
+                       <div class="widget-title-area">
+                           <h3 class="widget-title">
+                               About House Rent
+                           </h3><!-- /.widget-title -->
+                       </div><!-- /.widget-title-area -->
+                       <div class="widget-about-content">
+                           <img src="assets/images/footer-logo.png" alt="house" />
+                           <p>We Provide Premium Word Press, Ghost and HTML template. Our Perm tritium Templates is, develop gaped in a way so that the clients find  Support. Themes are developed in a way so that the clients find.</p>
+                           <a href="#" class="button">More</a>
+                       </div><!-- /.widget-content -->
+                   </div><!-- /.widget widget_about -->
+               </div><!-- /.col-md-4 -->
+               <div class="col-md-4">
+                   <div class="widget widget_place_category yellow-color">
+                       <div class="widget-title-area">
+                           <h3 class="widget-title">Place Category</h3><!-- /.widget-title -->
+                       </div><!-- /.widget-title-area -->
+                       <ul>
+                           <li>Flat for Rent <a href="#">Francis</a></li>
+                           <li>Flat for Rent <a href="#">Collins St</a></li>
+                           <li>Flat for Rent <a href="#">Rose Ln</a></li>
+                           <li>Flat for Rent <a href="#">Cosgrave Ln</a></li>
+                           <li>Flat for Rent <a href="#">Bourke St</a></li>
+                           <li>Flat for Rent <a href="#">Flienders Ln</a></li>
+                       </ul> 
+                   </div><!-- /.widget -->
+               </div><!-- /.col-md-4 -->
+               <div class="col-md-4">
+                   <div class="widget widget_instagram yellow-color">
+                       <div class="widget-title-area">
+                           <h3 class="widget-title">Instagram Image</h3><!-- /.widget-title -->
+                       </div><!-- /.widget-title-area -->
+                       <div class="instagram-image-content">
+                           <a href="#"><img src="assets/images/instagram/instagram-one.png" alt="" /></a>
+                           <a href="#"><img src="assets/images/instagram/instagram-two.png" alt="" /></a>
+                           <a href="#"><img src="assets/images/instagram/instagram-three.png" alt="" /></a>
+                           <a href="#"><img src="assets/images/instagram/instagram-four.png" alt="" /></a>
+                           <a href="#"><img src="assets/images/instagram/instagram-five.png" alt="" /></a>
+                           <a href="#"><img src="assets/images/instagram/instagram-six.png" alt="" /></a>
+
+                       </div><!-- /.instagram-image-content -->
+                   </div><!-- /.widget -->
+               </div><!-- /.col-md-4 -->
+           </div><!-- /.row -->
+           <div class="row">
+               <div class="col-md-12">
+                   <div class="bottom-content">
+                       <p>Copyright  &copy;2017 <a href="#">HTMLguru</a></p>
+                   </div><!-- /.bottom-top-content -->
+               </div><!-- /.col-md-12 -->
+           </div><!-- /.row -->
+       </div><!-- /.container -->
+    </footer><!-- /.footer-area -->
